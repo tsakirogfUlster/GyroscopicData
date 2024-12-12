b4e2df11cc8e2b21307504588055520bdf613b55
tsakirogfUlster <tsakiroglou-f@ulster.ac.uk>
2024-12-12 17:35:50 +0000

    Update test-sample.py

--- src/test-sample.py
+++ src/test-sample.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 class TestDataLoading(unittest.TestCase):
     def test_load_data(self):
-        file_path = 'src/1600g_filtered_labels.csv'
+        file_path = 'https://github.com/tsakirogfUlster/GyroscopicData/blob/master/src/1600g_filtered_labels.csv'
         data = pd.read_csv(file_path, header=None, delimiter=',', engine='python')
         self.assertFalse(data.empty, "The loaded DataFrame is empty.")
 
